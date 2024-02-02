6687b4b29b22ac9cd6687028154f32c61df5da70
Ahmed Hibet <60030432+Ahmed-Hibet@users.noreply.github.com>
2024-02-02 21:31:51 +0300

    Update 0014-longest-common-prefix.py

--- leetcode/0014-longest-common-prefix.py
+++ leetcode/0014-longest-common-prefix.py
@@ -4,13 +4,20 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        mn = min([len(s) for s in strs])
-        n = len(strs)
-        index = 0
-        for i in range(mn):
-            ch = strs[0][i]
-            for j in range(n):
-                if strs[j][i] != ch:
-                    return strs[0][:index]
-            index += 1
-        return strs[0][:index]
+        len_smallest_str = min([len(s) for s in strs])
+        size_of_strs = len(strs)
+        length_of_lcp = 0 # length of the longest common prefix
+        
+        # Check the first character of each string, 
+        # if all are similar, increament length_of_lcp by one 
+        # then repeat the same process with the next character of each string 
+        for i in range(len_smallest_str):
+            char_of_first_word = strs[0][i]
+            
+            for j in range(size_of_strs):
+                if strs[j][i] != char_of_first_word:
+                    # if any different character occured, return the answer
+                    return strs[0][:length_of_lcp]
+            length_of_lcp += 1
+        
+        return strs[0][:length_of_lcp]
